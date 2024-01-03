d027f77703590e7e63b16bd9f37b8baf90561a41
Eran Turgeman <erant@jfrog.com>
2024-01-03 09:58:16 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function main() {
     try {
         core.startGroup('Setup JFrog CLI');
         Utils.setCliEnv();
-        console.log("ERAN CHECK: ####################### 31 #######################") // TODO del
+        console.log("ERAN CHECK: ####################### 32 #######################") // TODO del
         let accessToken :string = await Utils.getJfrogAccessToken() //TODO make it return a struct with: username, password and access_token, jfrog url
         console.log(`ERAN CHECK: finished access token flow with access token: ${accessToken}`) // TODO del
         await Utils.getAndAddCliToPath();
