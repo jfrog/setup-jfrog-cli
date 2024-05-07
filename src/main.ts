567ef5e7f5020f916ebbfbcff31c31dea6e76f4f
Taylor Faubion <taylor.faubion@wbd.com>
2024-05-07 14:33:29 -0700

    add token to action output

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ async function main() {
         core.startGroup('Setup JFrog CLI');
         Utils.setCliEnv();
         let jfrogCredentials: JfrogCredentials = await Utils.getJfrogCredentials();
+        if (jfrogCredentials.accessToken != '') {
+            core.setOutput('token', jfrogCredentials.accessToken);
+        }
         await Utils.getAndAddCliToPath(jfrogCredentials);
         await Utils.configJFrogServers(jfrogCredentials);
     } catch (error) {
