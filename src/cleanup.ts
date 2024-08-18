160b1717dbcc5f0062160c9384bea32f5c284f1c
delarea <eyalde@jfrog.com>
2024-08-18 14:33:30 +0300

    Generate summary before server cleanup

--- src/cleanup.ts
+++ src/cleanup.ts
@@ -3,22 +3,19 @@ import { Utils } from './utils';
 
 async function cleanup() {
     try {
+        if (!core.getBooleanInput(Utils.JOB_SUMMARY_DISABLE)) {
+            core.startGroup('Generate Job Summary');
+            // Generate summary Markdown from data files
+            await Utils.runCli(['generate-summary-markdown']);
+            // Combine to a unified report
+            await Utils.setMarkdownAsJobSummary();
+            core.endGroup();
+        }
         core.startGroup('Cleanup JFrog CLI servers configuration');
         if (!Utils.addCachedCliToPath()) {
             return;
         }
         await Utils.removeJFrogServers();
-        if (!core.getBooleanInput(Utils.JOB_SUMMARY_DISABLE)) {
-            core.startGroup('Generate Job Summary');
-            // Generate summary Markdown from data files
-            try {
-                await Utils.runCli(['generate-summary-markdown']);
-            } catch (error) {
-                core.error(`Failed to generate summary markdown: ${error}`);
-            }
-            // Combine to a unified report
-            await Utils.setMarkdownAsJobSummary();
-        }
     } catch (error) {
         core.setFailed((<any>error).message);
     } finally {
