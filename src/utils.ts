61a3806b25bde57263c6e2fa8578002a5a721a9b
Eyal Ben Moshe <eyalbenmoshe@jfrog.com>
2024-12-23 12:15:18 +0200

    Update src/utils.ts
    Co-authored-by: Michael Sverdlov <sverdlov93@gmail.com>

--- src/utils.ts
+++ src/utils.ts
@@ -525,23 +525,14 @@ export class Utils {
 
     // Set usage variables to be captured by JFrog CLI.
     public static setUsageEnvVars(): void {
-        // Repository name, defaulting to an empty string if undefined.
-        const repoName: string = process.env.GITHUB_REPOSITORY || '';
-
-        // Workflow name, defaulting to an empty string if undefined.
-        const jobId: string = process.env.GITHUB_WORKFLOW || '';
-
-        // Run ID, defaulting to an empty string if undefined.
-        const runId: string = process.env.GITHUB_RUN_ID || '';
-
-        // Boolean flag indicating if JF_GIT_TOKEN is set.
-        const jfGitTokenSet: boolean = !!process.env.JF_GIT_TOKEN;
-
-        // Export environment variables for JFrog CLI usage.
-        core.exportVariable('JFROG_CLI_USAGE_JOB_ID', jobId);
-        core.exportVariable('JFROG_CLI_USAGE_RUN_ID', runId);
-        core.exportVariable('JFROG_CLI_USAGE_GIT_REPO', repoName);
-        core.exportVariable('JFROG_CLI_USAGE_GH_TOKEN_FOR_CODE_SCANNING_ALERTS_PROVIDED', jfGitTokenSet);
+    // Set the GitHub repository name or default to an empty string.
+    core.exportVariable('JFROG_CLI_USAGE_GIT_REPO', process.env.GITHUB_REPOSITORY ?? '');
+    // Set the GitHub workflow name or default to an empty string.
+    core.exportVariable('JFROG_CLI_USAGE_JOB_ID', process.env.GITHUB_WORKFLOW ?? '');
+    // Set the GitHub run ID or default to an empty string.
+    core.exportVariable('JFROG_CLI_USAGE_RUN_ID', process.env.GITHUB_RUN_ID ?? '');
+    // Indicate if JF_GIT_TOKEN is provided as an environment variable.
+    core.exportVariable('JFROG_CLI_USAGE_GH_TOKEN_FOR_CODE_SCANNING_ALERTS_PROVIDED', !!process.env.JF_GIT_TOKEN);
     }
 
     /**
